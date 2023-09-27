5b603fdc3aeb8a6f12f5e5e2c266ae57966df72f
elizabethjump <ejump@smcgov.org>
2023-09-27 15:00:15 -0700

    uploading function to create the full life table

---
+++ functions/create_life_table_function.R
@@ -0,0 +1,52 @@
+# function to make life table -------------------------------
+## data prep: from your dataset, rename the columns you want to look at to "deaths" and "population"
+
+# function for accumulate in life table
+fun_alive_int <- function(out, input) {
+  if(!is.na(input)) {
+    input*out 
+  } else {
+    return(done())
+  }
+}
+
+
+# function to make life table ----
+make_life_table <- function(data) {
+  life_table <- data %>%
+    mutate(int_width = case_when(age_cat == "0" ~ 1,
+                                 age_cat == "1-4" ~ 4,
+                                 age_cat == "90+" ~ 12.3,
+                                 TRUE ~ 5),
+           fract_surv = case_when(age_cat == "0" ~ 0.1,
+                                  TRUE ~ 0.5),
+           death_rate = deaths/population,
+           prob_dying = int_width*death_rate/(1+int_width*(1-fract_surv)*death_rate),
+           prob_surv = case_when(age_cat != "90+" ~ (1-prob_dying)),
+           num_alive_int = accumulate(.x = prob_surv,
+                                     .init = 100000,
+                                     .f = fun_alive_int),
+           num_dying_int = case_when(age_cat == "90+" ~ num_alive_int,
+                                     TRUE ~ num_alive_int - lead(num_alive_int)),
+           pers_yrs_lived_int = case_when(age_cat == "90+" ~ num_alive_int/death_rate, 
+                                          TRUE ~ int_width*(lead(num_alive_int)+(fract_surv*num_dying_int))),
+           pers_yrs_lived_past = accumulate(.x = pers_yrs_lived_int,
+                                            .f = `+`,
+                                            .dir = "backward"),
+           obs_le_int = pers_yrs_lived_past/num_alive_int,
+           sample_var = case_when(age_cat == "90+" ~ (death_rate*(1-death_rate)/population),
+                                  deaths == 0 ~ 0,
+                                  TRUE ~ (prob_dying^2*(1-prob_dying)/deaths)),
+           weighted_var = case_when(age_cat == "90+" ~ (num_alive_int^2)/death_rate^4*sample_var,
+                                    TRUE ~ (num_alive_int^2)*((1-fract_surv)*int_width+lead(obs_le_int))^2*sample_var),
+           sample_var_pers_yrs = accumulate(.x = weighted_var,
+                                            .f = `+`,
+                                            .dir = "backward"),
+           sample_var_obs_le = sample_var_pers_yrs/num_alive_int^2,
+           ci_low_95 = round(obs_le_int-(1.96*sqrt(sample_var_obs_le)), 1),
+           ci_high_95 = round(obs_le_int+(1.96*sqrt(sample_var_obs_le)), 1)) %>%
+    ungroup() 
+  
+  life_table
+  
+}
