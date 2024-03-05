3520b4a7983579ca614af90eb14800202b76b3ec
elizabethjump <ejump@smcgov.org>
2024-03-04 17:55:50 -0800

    updating comments in script

--- R/example-for-calculating-life-expectancy.R
+++ R/example-for-calculating-life-expectancy.R
@@ -15,18 +15,15 @@ source("R\\functions\\make-life-table-function.R")
 ## load data -----
 data <- rio::import("data/example-data.csv")
 
-## prep data ----
-data1 <- data %>%
-  mutate(start_age = as.numeric(str_extract(age_cat, "^\\d{1,2}"))) %>% # start age makes sure the age groups sort correctly
-  group_by(year) %>%
-  arrange(start_age)
-
 # get le estimates -----------------------------------
 ## create the whole life table -----
-le <- data1 %>%
-  make_life_table()
+le <- data %>%
+  mutate(start_age = as.numeric(str_extract(age_cat, "^\\d{1,2}"))) %>% # start age makes sure the age groups sort correctly
+  group_by(year) %>% # you can add other grouping variables (race, sex, etc here)
+  arrange(start_age) %>%
+  make_life_table() 
 
-## pull out what you want ----
+## pull out the life expectancy estimate ----
 le %>%
   filter(start_age == 0) %>%
   select(year, obs_le_int, ci_low_95, ci_high_95)
